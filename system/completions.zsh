317d7e0eefa65b8d77342cb52f98e19c88d608f7
Ulises Acosta <ulises.xsoft@gmail.com>
2023-08-24 15:55:25 -0700

    Newest changes August 2023

--- system/completions.zsh
+++ system/completions.zsh
@@ -1,6 +1,9 @@
 # Tab completion from both ends
 setopt completeinword
 
+# Load Git completion
+zstyle ':completion:*:*:git:*' script ~/.dotfiles/completions/git-completion.bash
+
 # Case-insensitive tab completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
--- system/prompt.zsh
+++ system/prompt.zsh
@@ -4,11 +4,11 @@ setopt PROMPT_SUBST
 autoload -U colors && colors
 autoload -U promptinit
 autoload -Uz compinit
-# if [ $(date +'%j') != $(stat -f '%Sm' -t '%j' ~/.zcompdump) ]; then
-#   compinit
-# else
-#   compinit -C
-# fi
+if [[ -n ${HOME}/.zcompdump(#qN.mh+24) ]]; then
+  compinit;
+else
+  compinit -C;
+fi;
 
 # Enable history
 HISTSIZE=10000
