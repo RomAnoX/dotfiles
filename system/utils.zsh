53c8bfc831a1d6671ae191ccca31063be98d8c57
Ulises Acosta <ulises.xsoft@gmail.com>
2024-02-23 17:58:32 -0700

    Updated stuff

--- system/utils.zsh
+++ system/utils.zsh
@@ -9,6 +9,3 @@ eval "$(direnv hook zsh)"
 
 # added by travis gem
 [ ! -s /Users/uacosta/.travis/travis.sh ] || source /Users/uacosta/.travis/travis.sh
-
-# Okta API Key for gimme-aws-creds
-export OKTA_API_KEY="00AQaJlD9-7QwC_XBUohk5_vrU4OtANJd0xDASqtY6"
